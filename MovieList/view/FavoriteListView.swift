153cca84c44d7363a004141d3451ab88eee9f3b7
Joakim Andersson <jandercode@gmail.com>
2022-03-16 14:16:26 +0100

    Added movieListView

---
+++ MovieList/view/FavoriteListView.swift
@@ -0,0 +1,20 @@
+//
+//  FavoriteListView.swift
+//  MovieList
+//
+//  Created by Joakim Andersson on 2022-03-16.
+//
+
+import SwiftUI
+
+struct FavoriteListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavoriteListView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteListView()
+    }
+}
